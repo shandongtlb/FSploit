2199b1e5fd7b9725a4644559b91c6abf0ce6211b
shandongtlb <40312509+shandongtlb@users.noreply.github.com>
2026-04-14 17:37:27 +0800

    清理调试工具

--- probe1.sh
+++
@@ -1,1 +0,0 @@
-find /sys/module/ -type f -name "*arp*offload*" -o -name "*apf*" -o -name "*bpf*" -o -name "*pkt_filter*" 2>/dev/null
--- probe2.sh
+++
@@ -1,1 +0,0 @@
-find /d/wlan0/ /d/wifi/ /sys/wifi/ /sys/class/net/wlan0/ -type f -name "*arp*" -o -name "*offload*" -o -name "*filter*" 2>/dev/null
